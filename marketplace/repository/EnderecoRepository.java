eef3454d5bf23746380a86811afcb7acd97e0636
Bruno Eugênio <135389183+brunoeugeniodev@users.noreply.github.com>
2025-12-02 16:09:04 -0300

    Delete marketplace/repository directory

--- marketplace/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.brunoeugeniodev.marketplace.repository;
-
-import io.github.brunoeugeniodev.marketplace.models.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
